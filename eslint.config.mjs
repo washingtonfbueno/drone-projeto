1ce11e213757687a7571476ae8f4ab5d4eb00fad
washingtonfbueno <washingtonfbueno@hotmail.com>
2025-03-28 13:44:28 -0300

    ignore interface empty

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
+	// import.meta.dirname is available after Node.js v20.11.0
+	baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
